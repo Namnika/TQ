81d328dcda3f4dd8b397b8be5ff1b6cd89b2a6cd
𝑵𝒂𝒎𝒏𝒊𝒌𝒂 𝑱𝒂𝒏𝒃𝒂𝒏𝒅𝒉𝒖 <janbandhunamnika@gmail.com>
2025-05-24 17:09:23 +0530

    Completed till Control flow

--- phase1_javascript.js
+++ phase1_javascript.js
@@ -2,12 +2,12 @@
 var a = "variable";
 console.log(a);
 
-// 2. 
+// 2. Create a variable with let and try updating its value.
 let b = 10;
 b = 20;
 console.log(b);
 
-// 3. 
+// 3. Create a constant using const and attempt to reassign it (observe the error).
 const c = 20;
 // c = 2;
 // console.log(c); // TypeError: Assignment to constant variable.
@@ -70,7 +70,7 @@ const obj = {
     b: 2
 }
 
-console.log(obj.a, obj.b); // { a: 1, b: 2 }
+console.log(obj.a, obj.b); // 1 2
 
 // 14.
 const arr = [1, 2, 3]
@@ -86,7 +86,7 @@ console.log(strToNum); // 10
 
 // 17.
 let numToStr = String(10);
-console.log(numToStr);
+console.log(typeof numToStr);
 
 // 18. 
 let strs = parseInt("100");
@@ -94,3 +94,158 @@ console.log(strs);
 
 let float = parseFloat("123.54");
 console.log(float);
+
+// 2. Operators
+
+// 19. 
+let n1 = 2, n2 = 3;
+
+const add = n1 + n2;
+const subtract = n1 - n2;
+const multi = n1 * n2;
+const div = n1 / n2;
+const modulus = n1 % n2;
+
+console.log(add, subtract, multi, div, modulus);
+
+// 20.
+const pow = n1 ** n2;
+console.log(pow);
+
+// 21.
+console.log(n1 += 1)
+console.log(n1 -= 1)
+console.log(n1 *= 2)
+console.log(n2 /= 3)
+console.log(n1 %= 2)
+
+// 22.
+console.log("1" == "1") // true, checking if value is equal or not
+console.log("1" === 1) // false, checking if data type and value both are equal or not
+
+// 23.
+
+console.log(10 != 10); // because 10 is equal to 10 = true, so !true means false
+console.log(10 !== "10");// here checking data type, so it is not equal. so, !false means true
+
+// 24. comparisons operator:  ==, ===, !=, !==, <, >, <=, >=
+console.log(n1 >= n2);
+
+// 25.
+if (n1 >= n2 && n1 <= n2) {
+    console.log("check both condition if true, it will return"); // not execute
+}
+
+if (n1 >= n2 || n1 <= n2) {
+    console.log("|| checks if any one condition is true, will return"); // execute
+}
+
+// 26.
+console.log(!false);
+
+// 27.
+console.log(n1 >= n2 ? "greater value" : "smaller value");
+
+// 3. Control Flow
+
+// 28. 
+let n = -2;
+if (n > 0) {
+    console.log("positive number");
+} else {
+    console.log("negative number");
+}
+
+// 29.
+let marks = 83;
+if (marks >= 70 && marks <= 90) {
+    console.log("Grade A");
+} else if (marks >= 65 && marks <= 70) {
+    console.log("Grade B");
+} else {
+    console.log("Grade C");
+}
+
+// 30.
+let message = "";
+let day = "Tues";
+
+switch (day) {
+    case "Mon":
+        message = "Today is Monday";
+        break;
+    case "Tues":
+        message = "Today is Tuesday";
+        break;
+    case "Wed":
+        message = "Today is Wednesday";
+        break;
+    case "Thurs":
+        message = "Today is Thursday";
+        break;
+    case "Fri":
+        message = "Today is Friday";
+        break;
+    case "Sat":
+        message = "Today is Saturday";
+        break;
+    case "Sun":
+        message = "Today is Sunday!";
+        break;
+}
+
+console.log(message);
+
+// 31.
+console.log("for loop printing value from 1 to 10");
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// 32.
+let i = 5;
+
+console.log("while loop count down");
+
+while (i >= 1) {
+    console.log(i);
+    i -= 1
+}
+
+// 33.
+console.log("do-while loop");
+
+let r = 5;
+do {
+    console.log(r);
+    r -= 1
+} while (r >= 1) {
+
+}
+
+// 34.
+
+// for...of used in iterating over array
+console.log("for...of loop iterating over an array");
+
+let arr1 = [1, 2, 3, 4, 5]
+
+for(let i of arr1){
+    console.log(i);
+}
+
+// 35.
+
+// for..in used in iterating over objects
+
+console.log("for..in used in iterating over objects");
+
+const objs = {
+    a: 1,
+    text: "some texts"
+}
+
+for (let i in objs){
+   console.log(objs[i]);
+}
